e98a7e8a7924bdbdfb1956f8eb25cd0779b1ab54
James Valles <jamesvallesdotcom@gmail.com>
2019-09-27 10:25:43 -0500

    Starting projecting adding comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
+//Import components here 
 import { Component } from '@angular/core';
 
+//Component decorator, where we can find template, CSS style sheet
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+//Component logic 
 export class AppComponent {
   title = 'mydist';
 }
