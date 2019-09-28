8c3da5c974c5ca9c5bfbe50e74932fe1d2929866
James Valles <jamesvallesdotcom@gmail.com>
2019-09-28 12:13:03 -0500

    Added google fonts and stylized entire page

--- src/app/messages.service.ts
+++ src/app/messages.service.ts
@@ -14,7 +14,7 @@ export class MessagesService {
   createMessages(){
     var message1 = new Message("5:45 p.m.", "Hey, can you do me favor and like Pierre's picture."); 
     var message2 = new Message("5:50 p.m.", "Thank you for liking his picture! You rock."); 
-    var message3 = new Message("6:00 p.m.", "What's for dinner?"); 
+    var message3 = new Message("6:00 p.m.", "We're on our way home. What's for dinner?"); 
     var message4 = new Message("6:15 p.m.", "Hot dogs!");  
     
     this.messageArray.addMessage(message1);
