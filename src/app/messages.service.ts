c9234e0a68c263bd78162f9058c35fdcdc423869
James Valles <jamesvallesdotcom@gmail.com>
2019-10-01 18:46:47 -0500

    Added user object to message service

--- src/app/messages.service.ts
+++ src/app/messages.service.ts
@@ -12,10 +12,13 @@ export class MessagesService {
   constructor() { }
 
   createMessages(){
-    var message1 = new Message("5:45 p.m.", "Hey, can you do me favor and like Pierre's picture.", "James"); 
-    var message2 = new Message("5:50 p.m.", "Thank you for liking his picture! You rock.", "Leo"); 
-    var message3 = new Message("6:00 p.m.", "We're on our way home. What's for dinner?", "James"); 
-    var message4 = new Message("6:15 p.m.", "Hot dogs!", "Leo");  
+    var user1 = new User("James");
+    var user2 = new User("Leo");
+
+    var message1 = new Message("5:45 p.m.", "Hey, can you do me favor and like Pierre's picture.", user1.name); 
+    var message2 = new Message("5:50 p.m.", "Thank you for liking his picture! You rock.", user2.name); 
+    var message3 = new Message("6:00 p.m.", "We're on our way home. What's for dinner?", user1.name); 
+    var message4 = new Message("6:15 p.m.", "Hot dogs!", user1.name);  
     
     this.messageArray.addMessage(message1);
     this.messageArray.addMessage(message2);
