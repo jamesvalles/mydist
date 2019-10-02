960980f39a93e5077126c555ba2c817157b80d3d
James Valles <jamesvallesdotcom@gmail.com>
2019-10-01 20:11:30 -0500

    Updated readme file

--- src/app/messages.service.ts
+++ src/app/messages.service.ts
@@ -18,7 +18,7 @@ export class MessagesService {
     var message1 = new Message("5:45 p.m.", "Hey, can you do me favor and like Pierre's picture.", user1.name); 
     var message2 = new Message("5:50 p.m.", "Thank you for liking his picture! You rock.", user2.name); 
     var message3 = new Message("6:00 p.m.", "We're on our way home. What's for dinner?", user1.name); 
-    var message4 = new Message("6:15 p.m.", "Hot dogs!", user1.name);  
+    var message4 = new Message("6:15 p.m.", "Hot dogs!", user2.name);  
     
     this.messageArray.addMessage(message1);
     this.messageArray.addMessage(message2);
